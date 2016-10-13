75660412c3fbb01ad019d3353536d0a17b2ad5c6
Charlie Gildawie <charlie@skyscanner.net>
2016-10-13 09:56:31 +0100

    inital framework to trigger travis-ci

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from waitress import serve
+import logging
+
+FORMAT = '%(asctime)-15s %(levelname)s:%(message)s'
+logging.basicConfig(format=FORMAT, datefmt='%Y-%m-%dT%I:%M:%S', level=logging.INFO)
+log = logging.getLogger(__name__)
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    log.info("Hello World!")
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port="8001")
