76aef1d3fccfdab9c4cfe59a7c85db373e4ac09f
Charlie Gildawie <charlieg@monzo.com>
2018-09-11 17:17:55 +0100

    sanic

---
+++ examples/sanic/app.py
@@ -0,0 +1,13 @@
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
+
+
+@app.route("/")
+async def hello(request):
+    return json({"hello": "world"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8001)
